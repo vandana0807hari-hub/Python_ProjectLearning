3be15e0fca26ec015eeaca46d997ef14de4386ff
Vandana <vandana0807.hari@gmail.com>
2025-10-03 11:50:57 -0500

    Read csv and json

---
+++ test_pandas.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+#df = pd.read_csv ("/Users/vanda/Documents/MyLearnings/PythonWorkspace/.venv/datafile.csv")
+#df = pd.read_json ("/Users/vanda/Documents/MyLearnings/PythonWorkspace/.venv/datafile.json")
+df = pd.read_csv ("/Users/vanda/Documents/MyLearnings/PythonWorkspace/.venv/datafile.txt",delimiter="|")
+
+print (df)
